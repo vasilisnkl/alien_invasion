77250b54b51f1812cfe60106ad9c071cc217bf94
Vasilis Nikolaou <146632898+vasilisnkl@users.noreply.github.com>
2023-10-01 17:47:28 +0300

    Add files via upload

---
+++ ship.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Ship(Sprite):
+
+    def __init__(self, ai_game):
+        """ship starting position"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        self.screen_rect = ai_game.screen.get_rect()
+
+        """load ship image and get its rect"""
+
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
+    
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+    
+    def update(self):
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+        
+        self.rect.x = self.x
+
+    def blitme(self):
+        """draw ship at current location"""
+        self.screen.blit(self.image, self.rect)
+
